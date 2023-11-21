2a2238e5817e3740ca899a9d055de30ca517ee03
Utente <Utente@DESKTOP-G6PC3T9>
2023-11-21 12:16:08 +0100

    Aggiungere i file di progetto.

---
+++ ImportCSV/Program.cs
@@ -0,0 +1,51 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using ImportCSV;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        using (var reader = new StreamReader("C:\\xampp\\htdocs\\file.csv"))
+        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+        {
+            var records = new List<RigaOrdineCsv>();
+            csv.Read();
+            csv.ReadHeader();
+            while (csv.Read())
+            {
+                var record = new RigaOrdineCsv();
+
+                record.order_id = csv.GetField<int>("order_id");
+                record.address_type = csv.GetField<string>("address_type");
+                record.first_name = csv.GetField<string>("first_name");
+                record.last_name = csv.GetField<string>("last_name");
+                record.company = csv.GetField<string>("company");
+                record.address_1 = csv.GetField<string>("address_1");
+                record.address_2 = csv.GetField<string>("address_2");
+                record.city = csv.GetField<string>("city");
+                record.postcode = csv.GetField<string>("postcode");
+                record.state = csv.GetField<string>("state");
+                record.phone = csv.GetField<string>("phone");
+                record.email = csv.GetField<string>("email");
+                record.order_item_type = csv.GetField<string>("order_item_type");
+                record.product_id = csv.GetField<int>("product_id");
+                record.variation_id = csv.GetField<int>("variation_id");
+                record.customer_id = csv.GetField<int>("customer_id");
+                record.date_created = csv.GetField<string>("date_created");
+                record.product_qty = csv.GetField<int>("product_qty");
+                record.product_net_revenue = csv.GetField<float>("product_net_revenue");
+                record.product_gross_revenue = csv.GetField<float>("product_gross_revenue");
+                record.coupon_amount = csv.GetField<int>("coupon_amount");
+                record.tax_amount = csv.GetField<float>("tax_amount");
+                record.shipping_amount = csv.GetField<float>("shipping_amount");
+                record.shipping_tax_amount = csv.GetField<float>("shipping_tax_amount");
+
+
+                records.Add(record);
+                Console.WriteLine(record);
+            }
+        }
+    }
+}
---
+++ ImportCSV/RigaOrdineCsv.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json.Serialization.Metadata;
+
+namespace ImportCSV
+{
+    public class RigaOrdineCsv
+    {
+        public int order_id { get; set; }
+        public string address_type { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set;}
+        public string company { get; set; }
+        public string address_1 { get; set; }
+        public string address_2 { get; set; }
+        public string city { get; set; }
+        public string postcode { get; set; }
+        public string state { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+        public string order_item_type { get; set; }
+        public int product_id { get; set; }  
+        public int variation_id { get; set }
+        public int customer_id { get; set; }
+        public string date_created { get; set; }
+        public int product_qty { get; set; }
+        public float product_net_revenue { get; set; }
+        public float product_gross_revenue { get; set; }
+        public int coupon_amount { get; set; }
+        public float tax_amount { get; set; }
+        public float shipping_amount { get; set; }
+        public float shipping_tax_amount { get; set; }
+
+        public override string ToString()
+        {
+            return $"Order: {order_id} Address Type: {address_type} First Name: {first_name} Last Name: {last_name} Company: {company} Address 1: {address_1} Address 2: {address_2} City: {city} Postcode: {postcode} State: {state} Phone: {phone} Email: {email} Order Item Type: {order_item_type} Product id: {product_id} Variation ID: {variation_id} Customer ID: {customer_id} Date Created: {date_created} Product QTY: {product_qty} Product Net Revenue: {product_net_revenue} Product Gross Revenue: {product_gross_revenue} Coupon Amount: {coupon_amount} Tax Amount: {tax_amount} Shipping Amount: {shipping_amount} Shipping Tax Amount: {shipping_tax_amount}";
+        }
+    }  
+}
